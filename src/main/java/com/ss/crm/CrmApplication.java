9dcff06647a3b564d3e1a39c70ed9c8ff9ca1e38
bl-z <143209786@qq.com>
2019-01-11 18:11:11 +0800

    添加

--- src/main/java/com/ss/crm/CrmApplication.java
+++ src/main/java/com/ss/crm/CrmApplication.java
@@ -2,7 +2,6 @@ package com.ss.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class CrmApplication {
 
