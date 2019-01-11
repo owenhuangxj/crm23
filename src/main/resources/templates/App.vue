3546082b3e21549aeb6af23f246860de86eaf407
owenhuangxj <wangluoke007@163.com>
2019-01-11 13:27:58 +0800

    添加了Vue的组件测试，包括App.vue,test_vue_module.html，用于测试vue组件的引用，即.vue文件的引用！

---
+++ src/main/resources/templates/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        主页 {{who}}
+    </div>
+</template>
+
+<script>
+    module.exports =  {
+        data:function ()
+        {
+            return{
+                who:"Hello World!"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
