d4e1b05aa185ec641fa5e206a0babd16063e7d46
bl-z <143209786@qq.com>
2019-01-11 19:24:56 +0800

    删除test的allotlog

--- src/main/java/com/ss/crm/entity/TrackInfo.java
+++ src/main/java/com/ss/crm/entity/TrackInfo.java
@@ -1,7 +1,20 @@
 package com.ss.crm.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("t_track_info")
 public class TrackInfo extends Model<TrackInfo> {
+
+    @TableId
+    private Long trackId;
+
 
 }
--- src/test/java/com/ss/crm/CrmApplicationTests.java
+++ src/test/java/com/ss/crm/CrmApplicationTests.java
@@ -1,12 +1,9 @@
 package com.ss.crm;
 
-import com.ss.crm.entity.AllotLog;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,9 +15,7 @@ public class CrmApplicationTests {
 
     @Test
     public void select(){
-        AllotLog allotLog = new AllotLog();
-        List<AllotLog> allotLogs = allotLog.selectAll();
-        System.out.println(allotLogs);
+
     }
 
 
