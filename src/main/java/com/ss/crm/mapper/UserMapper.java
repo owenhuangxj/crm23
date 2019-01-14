4bb798ec908a21cc556d555c8884dfe2577de398
bookstore <2535545766@qq.com>
2019-01-14 09:53:10 +0800

    User实体类，登陆注册页面修改

--- src/main/java/com/ss/crm/mapper/UserMapper.java
+++ src/main/java/com/ss/crm/mapper/UserMapper.java
@@ -1,5 +1,9 @@
 package com.ss.crm.mapper;
 
-public interface UserMapper {
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ss.crm.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
 
 }
