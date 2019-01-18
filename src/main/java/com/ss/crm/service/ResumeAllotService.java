285c4d1855787c20661eef11827ff7b64df486a0
SummersKing <moshenwuji@live.com>
2019-01-18 16:48:26 +0800

    常量包

--- src/main/java/com/ss/crm/service/ResumeAllotService.java
+++ src/main/java/com/ss/crm/service/ResumeAllotService.java
@@ -1,5 +1,10 @@
 package com.ss.crm.service;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.ss.crm.entity.Student;
+import org.apache.ibatis.annotations.Param;
+
 /**
  * @Author: 王顺坤
  * @Date: 2019/1/18 16:37
@@ -8,4 +13,6 @@ package com.ss.crm.service;
 public interface ResumeAllotService {
 
 
+    /*多条件分页查询*/
+    IPage<Student> selectPage(IPage<Student> page, @Param("ew") Wrapper<Student> queryWrapper);
 }
