34f04e30ec9485ff3e5fa6ebd4417683a8a70fef
bookstore <2535545766@qq.com>
2019-01-18 16:51:31 +0800

    常量类

---
+++ src/main/java/com/ss/crm/constant/AllConstant.java
@@ -0,0 +1,6 @@
+package com.ss.crm.constant;
+
+public class AllConstant {
+    public static final String APPSERVERADDRESS="http://localhost";
+//	public static final String APPSERVERADDRESS="http://www.superduck.xyz";
+}
