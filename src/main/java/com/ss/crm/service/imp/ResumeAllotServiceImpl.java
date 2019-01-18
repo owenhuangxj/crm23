a044609e1561819d312b06828bff820d31c03514
SummersKing <moshenwuji@live.com>
2019-01-18 16:45:43 +0800

    常量包

--- src/main/java/com/ss/crm/service/imp/ResumeAllotServiceImpl.java
+++ src/main/java/com/ss/crm/service/imp/ResumeAllotServiceImpl.java
@@ -11,9 +11,6 @@ import java.util.List;
  */
 public class ResumeAllotServiceImpl {
 
-    public  static List<String> CONSUME_NEEDED_COLUMN=new ArrayList<>( Arrays.asList("姓名","性别","手机","出生日期","家庭地址","职能/职位","自我评价","工作经验","项目经验","培训经历"));
-      public  static List<Integer> CONSUME_51JOB_ROW_INDEX=new ArrayList<>(Arrays.asList(9,10,11,13,22,35,39,41,43,52));
-    /*public  static List<Integer> CONSUME_51JOB_ROW_INDEX=new ArrayList<>(Arrays.asList(9,10,11,13));*/
-    public static List<Integer> CONSUME_51JOB_COLUMN_INDEX=new ArrayList<>(Arrays.asList(0,7,8));
+
 
 }
