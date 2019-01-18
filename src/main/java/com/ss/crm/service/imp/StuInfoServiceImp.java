0e1ada8686d2de3d631c4619fa1647b0f0cbf962
bl-z <143209786@qq.com>
2019-01-18 16:28:39 +0800

    2019 1-18 16:28

--- src/main/java/com/ss/crm/service/imp/StuInfoServiceImp.java
+++ src/main/java/com/ss/crm/service/imp/StuInfoServiceImp.java
@@ -99,6 +99,6 @@ public class StuInfoServiceImp implements StuInfoService {
      * @return
      */
     public Student getStu(StudentInfo info) {
-        return new Student(info.getStuNumber(), info.getStuName(), DateUtil.getDateTime(), info.getStuSex(), info.getStuImportance(), info.getStuPhoneNum(), info.getStuQq(), info.getStuLevel(), info.getStuAddress(), info.getStuChannel(), info.getStuSource(), info.getStuEvaluation(), info.getStuWork(), info.getStuProject(), info.getStuEducation(), info.getStuTrained(), info.getStuCredentials(), info.getStuPerformance(), getConsultId(info), getTeacherId(info));
+        return new Student(info.getStuNumber(), DateUtil.getDateTime(), info.getStuName(), info.getStuSex(), info.getStuImportance(), info.getStuPhoneNum(), info.getStuQq(), info.getStuLevel(), info.getStuAddress(), info.getStuChannel(), info.getStuSource(), info.getStuEvaluation(), info.getStuWork(), info.getStuProject(), info.getStuEducation(), info.getStuTrained(), info.getStuCredentials(), info.getStuPerformance(), getConsultId(info), getTeacherId(info));
     }
 }
