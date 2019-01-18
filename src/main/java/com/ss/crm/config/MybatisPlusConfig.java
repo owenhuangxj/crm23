d28fca8c8e1547df9809f5f34a9c194beb1c1c78
SummersKing <moshenwuji@live.com>
2019-01-18 16:42:22 +0800

    提交了一些

--- src/main/java/com/ss/crm/config/MybatisPlusConfig.java
+++ src/main/java/com/ss/crm/config/MybatisPlusConfig.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class MybatisPlusConfig {
 
     /**
-     * 分页插件
+     * 分页配置
      */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
